e1601901244f617a51fe818f0bc7395122786ab3
kagamma <kagamma.km@gmail.com>
2018-05-17 10:50:22 +0700

    Blog without images

---
+++ Presentation/Nop.Web/Components/BlogHomePage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Blogs;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Media;
+using Nop.Services.Blogs;
+using Nop.Services.Common;
+using Nop.Services.Events;
+using Nop.Services.Helpers;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Media;
+using Nop.Services.Messages;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Nop.Web.Factories;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc;
+using Nop.Web.Framework.Mvc.Filters;
+using Nop.Web.Framework.Mvc.Rss;
+using Nop.Web.Framework.Security;
+using Nop.Web.Framework.Security.Captcha;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.Blogs;
+
+
+namespace Nop.Web.Controllers
+{
+    public class BlogHomePageViewComponent : NopViewComponent
+    {
+        private readonly IBlogModelFactory _blogModelFactory;
+        private readonly BlogSettings _blogSettings;
+
+        public BlogHomePageViewComponent(IBlogModelFactory blogModelFactory,
+            BlogSettings blogSetting)
+        {
+            this._blogModelFactory = blogModelFactory;
+            this._blogSettings = blogSetting;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var command = new BlogPagingFilteringModel();
+            var model = _blogModelFactory.PrepareBlogPostListModel(command);
+            return View(model);
+        }
+    }
+}
